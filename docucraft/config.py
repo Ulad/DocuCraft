799f6614ca9234379d493d4378a7dab3deb74766
Levchenko V.V. <levchenko19_97@mail.ru>
2025-03-16 16:35:46 +0300

    (refactor) update config

--- docucraft/config.py
+++ docucraft/config.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from pydantic_settings import BaseSettings
 
 from pathlib import Path
@@ -10,8 +12,12 @@ class Settings(BaseSettings):
     BASE_DIR: Path = Path(__file__).parent.parent
     PATH_TO_EXCEL: Path = BASE_DIR / 'tests' / 'templates' / 'dummy_excel.xlsx'
     TABLE_NAME_IN_EXCEL: str = 'dummy_data'
-    GROUP_BY_COLUMN: str = 'address'
-    PATH_TO_DOCX_TPL: Path = BASE_DIR / 'tests' / 'dummy_doc.docx'
+    GROUP_BY_COLUMN: str = 'Address'
+    PATH_TO_DOCX_TPL: Path = BASE_DIR / 'tests' / 'templates' / 'dummy_doc.docx'
+    OUTPUT_DIR_DOCX: Path = BASE_DIR / 'reports'
 
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self.OUTPUT_DIR_DOCX.mkdir(parents=True, exist_ok=True)
 
 settings = Settings()
