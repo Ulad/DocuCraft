348883aab453a69682cedef71588d7882168eb55
Levchenko V.V. <levchenko19_97@mail.ru>
2025-03-16 13:49:53 +0300

    (feat) main entrypoint

--- docucraft/main.py
+++ docucraft/main.py
@@ -11,11 +11,20 @@ The process involves 2 main components:
         value - another dictionary where each key corresponds to a placeholder in the template
 """
 
+from docucraft.src.data_loader import read_excel_table
+from docucraft.src.utils import get_peak_memory_usage, log_timeit
+from docucraft.src import logger
+from docucraft.config import settings as cfg
 
+
+@log_timeit
 def main() -> None:
     """Orchestrates the entire workflow."""
-    print("Hello from docucraft!")
+    read_excel_table(cfg.PATH_TO_EXCEL, table_name="dummy_data")
+
+    logger.info(get_peak_memory_usage())
 
 
 if __name__ == "__main__":
+    logger.info(cfg.model_dump_json(indent=4, exclude={'BASE_DIR'}))
     main()
