c8cdf86bddd2b7d081babdc08b67217bb04fb231
Levchenko V.V. <levchenko19_97@mail.ru>
2025-04-04 22:49:26 +0300

    (refactor) update pydantic settings to follow best practices

--- docucraft/config.py
+++ docucraft/config.py
@@ -6,26 +6,35 @@ Environment variables will always take precedence over the values loaded from th
 Documentation: https://docs.pydantic.dev/latest/concepts/pydantic_settings/
 """
 
-from typing import Any
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import FilePath, model_validator
 
 from pathlib import Path
 
 BASE_DIR: Path = Path(__file__).parent.parent
 
+
 class Settings(BaseSettings):
     """Class for main settings"""
-    model_config = SettingsConfigDict(env_file=BASE_DIR / ".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=BASE_DIR / ".env",
+        env_file_encoding="utf-8",
+        str_min_length=1,
+        str_max_length=50,
+        frozen=True
+    )
 
-    PATH_TO_EXCEL: Path = BASE_DIR / 'tests' / 'templates' / 'dummy_excel.xlsx'
+    PATH_TO_EXCEL: FilePath = BASE_DIR / 'tests' / 'templates' / 'dummy_excel.xlsx'
     TABLE_NAME_IN_EXCEL: str = 'dummy_data'
     GROUP_BY_COLUMN: str = 'Address'
     PATH_TO_DOCX_TPL: Path = BASE_DIR / 'tests' / 'templates' / 'dummy_doc.docx'
     OUTPUT_DIR_DOCX: Path = BASE_DIR / 'reports'
 
-    def __init__(self, **kwargs: Any) -> None:
-        super().__init__(**kwargs)
+    @model_validator(mode='after')
+    def validate_and_create_dirs(self) -> 'Settings':
+        """Validate and create directories"""
         self.OUTPUT_DIR_DOCX.mkdir(parents=True, exist_ok=True)
+        return self
+
 
 settings = Settings()
