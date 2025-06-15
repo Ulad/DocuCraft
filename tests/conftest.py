8c0767c87c4a8588367e4a427f58bbc1a98753d5
Levchenko V.V. <levchenko19_97@mail.ru>
2025-06-15 12:16:02 +0300

    (refactor) better logging handling in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
-from pytest_mock import MockerFixture
-from pytest import fixture
+from pytest import fixture, LogCaptureFixture
+
+import logging
 
 
-@fixture(autouse=True, scope='function')
-def _suppress_logs(mocker: MockerFixture) -> None:
-    """Globally suppress logger output for all tests in this module"""
-    mocker.patch("docucraft.src.docx_creater.logger.warning")
+@fixture(autouse=True)
+def test(caplog: LogCaptureFixture) -> None:
+    caplog.set_level(logging.CRITICAL)
