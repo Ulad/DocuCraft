3c1b01fcc656176f59d3818b7c978f612305ac03
Ulad <79265417+Ulad@users.noreply.github.com>
2026-01-13 16:47:25 +0300

    Update custom_types.py

--- docucraft/src/custom_types.py
+++ docucraft/src/custom_types.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, Union
 from pathlib import Path
 
 from pydantic import StringConstraints
@@ -6,12 +6,12 @@ from docxtpl import RichText, Listing, Subdoc, InlineImage # type: ignore
 
 # Only allow alphanumeric and underscores https://jinja.palletsprojects.com/en/stable/api/#notes-on-identifiers
 type JinjaTag = Annotated[str, StringConstraints(pattern=r'^[a-zA-Z_][a-zA-Z0-9_]*$')]
-type JinjaValue = (
-        str | float | None
-        | list[JinjaContext]  # For tables
-        | JinjaContext  # For nested data
-        | list[str]  # For dynamic columns mostly
-        | RichText | InlineImage | Subdoc | Listing  # For docxtpl types
-)
+type JinjaValue = Union[ # using '|' generates Any somehow (try 3.14 lazy evaluation)
+        str, float, None,
+        list[JinjaContext],  # For tables
+        JinjaContext,  # For nested data
+        list[str],  # For dynamic columns mostly
+        RichText, InlineImage, Subdoc, Listing  # For docxtpl types
+]
 type JinjaContext = dict[JinjaTag, JinjaValue]
 type FilesDict = dict[Path, JinjaContext]
